adc4cf89d34f13d6f739ccad7411e6010902af6b
Maria Sorokina <ruafelianna@ya.ru>
2025-07-03 13:29:59 +0300

    Update example

--- sample.py
+++ sample.py
@@ -1,4 +1,6 @@
-from SilvaViridis.Python.WinAPI.Wrapper import USBDeviceManager, Types
+from SilvaViridis.Python.WinAPI.Wrapper import USBDeviceManager, Types, COMPortDeviceManager
+
+comports = list(COMPortDeviceManager.enumerate_comport_devices())
 
 nodes = USBDeviceManager.build_usb_tree()
 
@@ -7,7 +9,15 @@ def print_nodes(nodes : list[USBDeviceManager.USBNode]):
         if isinstance(node.device, USBDeviceManager.USBPort):
             s = f"Port {node.device.index + 1:02}"
         else:
-            s = f"{node.device.properties[Types.DevProperties.DEVICEDESC]}"
+            s = f"{node.device.properties[Types.DevProperties.DEVICEDESC]} [{node.device.id}]"
+
+            comport = next(
+                (p for p in comports if p.parent.lower() == node.device.id.lower()),
+                None,
+            )
+
+            if comport is not None:
+                s = f"[{comport.get_port_name()}] {s}"
         print(f"{"  " * node.level} {s}")
         print_nodes(node.children)
 
